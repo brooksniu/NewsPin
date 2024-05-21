aa8a63d79375540bd15182f739c04f7113c5a3a7
Xin Sheng <xisheng@ucsd.edu>
2024-05-21 13:29:35 -0700

    Favorite button

---
+++ dev-mockNewsList.js
@@ -0,0 +1,67 @@
+const mockArticles = [{
+    "title": "Warnings grow of blackout risks in summers ahead as NSW looks at extending Eraring coal plant",
+    "description": "The national energy market operator has issued new warnings of possible summer blackouts in coming years, due to delays in key transmission projects and looming retirements of coal generators.",
+    "content": "In short: Energy market operator AEMO has warned delays in transmission projects and looming retirements of coal generators risk blackouts in future.\nEnergy market operator AEMO has warned delays in transmission projects and looming retirements of co... [3808 chars]",
+    "url": "https://www.abc.net.au/news/2024-05-21/aemo-warns-of-increased-blackout-risks-over-coming-summers/103872480",
+    "image": "https://live-production.wcms.abc-cdn.net.au/42b5b5e6f807f473f40c2b52ded5988a?impolicy=wcms_watermark_news&cropH=1688&cropW=3000&xPos=0&yPos=154&width=862&height=485&imformat=generic",
+    "publishedAt": "2024-05-20T22:47:12Z",
+    "source": {
+        "name": "ABC News",
+        "url": "https://www.abc.net.au"
+    }
+}, {
+    "title": "Australia news LIVE: ICC seeks arrest warrants for Israeli and Hamas leaders; Job market tightens as interest rates bite",
+    "description": "Follow today’s national news headlines with our live blog.",
+    "content": "Prime Minister Anthony Albanese has called on Opposition Leader Peter Dutton to share more detail from his budget reply last week, saying specifics are needed if they want to be taken seriously.\nDuring Dutton’s budget reply speech, the opposition lea... [357 chars]",
+    "url": "https://www.smh.com.au/national/australia-news-live-icc-seeks-arrest-warrants-for-israeli-and-hamas-leaders-job-market-tightens-as-interest-rates-bite-20240521-p5jf8q.html",
+    "image": "https://static.ffx.io/images/$zoom_0.3512%2C$multiply_0.7554%2C$ratio_1.777778%2C$width_1059%2C$x_177%2C$y_0/t_crop_custom/q_86%2Cf_auto/t_smh_live_no_age_social_wm/07144b7a371791de08435528ca9b85cd4f50127b",
+    "publishedAt": "2024-05-20T22:45:42Z",
+    "source": {
+        "name": "Sydney Morning Herald",
+        "url": "https://www.smh.com.au"
+    }
+}, {
+    "title": "GTA 6 Could Be The First Game To Push Past $120 AUD",
+    "description": "Would you pay up to $140 AUD for a standard edition copy of GTA 6? It seems like Take Two CEO Strauss Zelnick is really hoping you will!",
+    "content": "It’s only been about two years or so since the video game industry began charging $US70 for big games instead of the usual $US60, due to game development costs increasing (and greed). This shift, understandably, didn’t go over well with players as th... [6675 chars]",
+    "url": "https://www.kotaku.com.au/2024/05/gta-6-could-be-the-first-game-to-push-past-120-aud/",
+    "image": "https://www.kotaku.com.au/wp-content/uploads/2024/05/5a2713e58e611fa29329ece80f4fbb74.jpg?quality=75",
+    "publishedAt": "2024-05-20T22:40:21Z",
+    "source": {
+        "name": "Kotaku Australia",
+        "url": "https://www.kotaku.com.au"
+    }
+},{
+    "title": "Astronomers Reveal Stunning New Image of Triple-Star System Gleaming in Space",
+    "description": "In a world that seems to be switching focus from the Hubble Space Telescope to the James Webb Space Telescope, Hubble still reminds us it's there.",
+    "content": "In a world that seems to be switching focus from the Hubble Space Telescope to the James Webb Space Telescope, Hubble still reminds us it's there.\nAnother amazing image has been released that shows the triple-star system HP Tau, HP Tau G2, and HP Tau... [2632 chars]",
+    "url": "https://www.sciencealert.com/astronomers-reveal-stunning-new-image-of-triple-star-system-gleaming-in-space",
+    "image": "https://www.sciencealert.com/images/2024/05/Triple-StarSystemInSpace.jpg",
+    "publishedAt": "2024-05-20T23:01:20Z",
+    "source": {
+        "name": "ScienceAlert",
+        "url": "https://www.sciencealert.com"
+    }
+},{
+    "title": "Punjab PA passes Defamation Bill 2024 amid opposition protest",
+    "description": "LAHORE: The Punjab government on Monday passed the Defamation Bill 2024 in Punjab Assembly while rejecting all the...",
+    "content": "LAHORE: The Punjab government on Monday passed the Defamation Bill 2024 in Punjab Assembly while rejecting all the amendments of the opposition and amidst the protest.\nThe bill was presented by Parliamentary Affairs Minister Mujtaba Shuja-ur-Rehman. ... [3844 chars]",
+    "url": "https://www.brecorder.com/news/40304425/punjab-pa-passes-defamation-bill-2024-amid-opposition-protest",
+    "image": "https://i.brecorder.com/large/2024/05/664bcdf571c08.jpg",
+    "publishedAt": "2024-05-20T22:39:34Z",
+    "source": {
+        "name": "Business Recorder",
+        "url": "https://www.brecorder.com"
+    }
+},{
+    "title": "Federal Budget on June 7th",
+    "description": "ISLAMABAD: The federal government will present the budget for the next financial year 2024-25 in the National...",
+    "content": "ISLAMABAD: The federal government will present the budget for the next financial year 2024-25 in the National Assembly on June 7 (Friday). The upcoming budget session will start on June 6 (Thursday) and the federal government will present Budget-2024... [1058 chars]",
+    "url": "https://www.brecorder.com/news/40304435/federal-budget-on-june-7th",
+    "image": "https://i.brecorder.com/large/2024/05/664bcc12a9a3f.jpg",
+    "publishedAt": "2024-05-20T22:39:32Z",
+    "source": {
+        "name": "Business Recorder",
+        "url": "https://www.brecorder.com"
+    }
+}];
